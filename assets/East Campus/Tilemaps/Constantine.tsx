55cc69c02da2044286c01819a04078ecb9d75a97
Sam Hartley <121134897+samh366@users.noreply.github.com>
2024-03-19 23:08:19 +0000

    Added accomodation sprite

---
+++ assets/East Campus/Tilemaps/Constantine.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Constantine" tilewidth="24" tileheight="24" tilecount="165" columns="15">
+ <image source="../Textures/Constantine.png" width="372" height="273"/>
+</tileset>
